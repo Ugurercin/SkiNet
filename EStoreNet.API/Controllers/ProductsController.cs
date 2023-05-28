890189a60ad7b7d33504094c3baa1a4e8fc523f2
ugur <ercinugur@gmail.com>
2023-05-28 16:34:42 +0300

    initial commit

---
+++ EStoreNet.API/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using EStoreNet.Core.Entitites;
+using EStoreNet.Infrastructure.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EStoreNet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct(Product product)
+        {
+            context.Products.Add(product);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
