3513cb87f55195489b1d6043fc89f0ab11b7b067
ugur <ercinugur@gmail.com>
2023-05-31 20:27:57 +0300

    edit

--- EStoreNet.Infrastructure/Concrete/ProductRepository.cs
+++ EStoreNet.Infrastructure/Concrete/ProductRepository.cs
@@ -18,8 +18,21 @@ namespace EStoreNet.Infrastructure.Concrete
             this.context = context;
         }
 
-        public async Task<Product> GetProductByIdAsync(int id) => await context.Products.FindAsync(id);
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.productBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync() => await context.Products.ToListAsync();
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await context.Products
+                 .Include(p => p.ProductType)
+                 .Include(p => p.productBrand)
+                 .ToListAsync();
+        }
+
     }
 }
