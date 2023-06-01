271e9b9b3b19e184416d27124716db9970792c7d
ugur <ercinugur@gmail.com>
2023-06-01 15:46:52 +0300

    Product DTO and auto mapper added

--- EStoreNet.API/Controllers/ProductsController.cs
+++ EStoreNet.API/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using EStoreNet.Core.Abstract;
+﻿using AutoMapper;
+using EStoreNet.API.DTOs;
+using EStoreNet.Core.Abstract;
 using EStoreNet.Core.Entitites;
 using EStoreNet.Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
@@ -12,28 +14,32 @@ namespace EStoreNet.API.Controllers
         private readonly IGenericRepository<Product> productsRepo;
         private readonly IGenericRepository<ProductBrand> productBrandRepo;
         private readonly IGenericRepository<ProductType> productTypeRepo;
+        private readonly IMapper mapper;
 
         public ProductsController(IGenericRepository<Product> productsRepo, IGenericRepository<ProductBrand> productBrandRepo,
-                                  IGenericRepository<ProductType> productTypeRepo)
+                                  IGenericRepository<ProductType> productTypeRepo,IMapper mapper)
         {
             this.productsRepo = productsRepo;
             this.productBrandRepo = productBrandRepo;
             this.productTypeRepo = productTypeRepo;
+            this.mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
         {
             var specification = new ProductsWithTypesAndBrandsSpecification();
             var products = await productsRepo.ListAsync(specification);
 
-            return Ok(products);
+            return Ok(mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var specification = new ProductsWithTypesAndBrandsSpecification(id);
-            return await productsRepo.GetEntityWithSpec(specification);
+            var products =  await productsRepo.GetEntityWithSpec(specification);
+
+            return mapper.Map<Product, ProductToReturnDto>(products);
         }
         [HttpGet("brands")]
         public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
---
+++ EStoreNet.API/Helpers/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EStoreNet.API.DTOs;
+using EStoreNet.Core.Entitites;
+
+namespace EStoreNet.API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(d => d.productBrand, o => o.MapFrom(s => s.productBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+
+
+        }
+
+    }
+}
